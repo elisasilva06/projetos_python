7f19bcf4223c8457935e9667be85e050c45ebb85
ALEMA <ana.eosilva@aluno.obompastor.com.br>
2025-03-12 22:21:49 -0300

    cadastro
    pagina de cadastro usando PySimpleGUI

---
+++ intermediario/cadastro.py
@@ -0,0 +1,27 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+#layout
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuário'),sg.Input(key='usuario')],
+    [sg.Text('Senha'), sg.Input(key='senha' , password_char='*')],
+    [sg.Checkbox('Salvar o login? ')],
+    [sg.Button('Entrar')]
+]
+#Janela
+janela = sg.Window('Tela de Login', layout)
+#ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'elisa' and valores['senha'] == '123456':
+            print('Bem-vindo a ELISAKOOK')
+
+
+
+
+
+
+
