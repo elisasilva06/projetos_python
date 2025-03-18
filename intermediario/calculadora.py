03efc2cce3ec0de2c30e345138fbe8b90be4daa2
ALEMA <ana.eosilva@aluno.obompastor.com.br>
2025-03-18 17:12:38 -0300

    Update calculadora.py

--- intermediario/calculadora.py
+++ intermediario/calculadora.py
@@ -5,9 +5,14 @@ sg.theme('LightBlue')
 
 
 layout = [
-    [C , ( , ) , /],
-    [7 , 8 , 9 , *],
-    [4 , 5 , 6 , -],
-    [1 , 2 , 3 , +],
-    [0 , . , % , =]
+    [sg.Multiline("", size=(25, 5), key="-display-", justification="r", font="digit 20 ", no_scrollbar=True)],
+    [sg.Button("C", size=(3,3)), sg.Button("(", size=(3,3)), sg.Button(")", size=(3,3)), sg.Button("/", size=(3,3))],
+    [sg.Button("7", size=(3,3)), sg.Button("8", size=(3,3)), sg.Button("9", size=(3,3)), sg.Button("*", size=(3,3))],
+    [sg.Button("4", size=(3,3)), sg.Button("5", size=(3,3)), sg.Button("6", size=(3,3)), sg.Button("-", size=(3,3))],
+    [sg.Button("1", size=(3,3)), sg.Button("2", size=(3,3)), sg.Button("3", size=(3,3)), sg.Button("+", size=(3,3))],
+    [sg.Button("0", size=(3,3)), sg.Button(".", size=(3,3)), sg.Button("%", size=(3,3)), sg.Button("=", size=(3,3))]   
 ]
+
+window = sg.Window("CALCULADORA", layout=layout, element_justification="center")
+
+window.read()
