1a01095d934d1030e9c7b9e430a435876225b40f
ALEMA <ana.eosilva@aluno.obompastor.com.br>
2025-03-18 17:50:35 -0300

    Update calculadora.py

--- intermediario/calculadora.py
+++ intermediario/calculadora.py
@@ -4,15 +4,17 @@ import re, sys
 sg.theme('LightBlue')
 
 
+
+
 layout = [
-    [sg.Multiline("", size=(25, 5), key="-display-", justification="r", font="digit 20 ", no_scrollbar=True)],
-    [sg.Button("C", size=(3,3)), sg.Button("(", size=(3,3)), sg.Button(")", size=(3,3)), sg.Button("/", size=(3,3))],
-    [sg.Button("7", size=(3,3)), sg.Button("8", size=(3,3)), sg.Button("9", size=(3,3)), sg.Button("*", size=(3,3))],
-    [sg.Button("4", size=(3,3)), sg.Button("5", size=(3,3)), sg.Button("6", size=(3,3)), sg.Button("-", size=(3,3))],
-    [sg.Button("1", size=(3,3)), sg.Button("2", size=(3,3)), sg.Button("3", size=(3,3)), sg.Button("+", size=(3,3))],
-    [sg.Button("0", size=(3,3)), sg.Button(".", size=(3,3)), sg.Button("%", size=(3,3)), sg.Button("=", size=(3,3))]   
+    [sg.Multiline("", size=(20, 3), key="-display-", justification="r", font="Helvetica 20", no_scrollbar=True)],
+    [sg.Button("C", size=(2,2), button_color=('DarkBlue')), sg.Button("(", size=(2,2), button_color=('DarkBlue')), sg.Button(")", size=(2,2), button_color=('DarkBlue')), sg.Button("/", size=(2,2), button_color=('DarkBlue'))],
+    [sg.Button("7", size=(2,2)), sg.Button("8", size=(2,2)), sg.Button("9", size=(2,2)), sg.Button("*", size=(2,2), button_color=('DarkBlue'))],
+    [sg.Button("4", size=(2,2)), sg.Button("5", size=(2,2)), sg.Button("6", size=(2,2)), sg.Button("-", size=(2,2), button_color=('DarkBlue'))],
+    [sg.Button("1", size=(2,2)), sg.Button("2", size=(2,2)), sg.Button("3", size=(2,2)), sg.Button("+", size=(2,2), button_color=('DarkBlue'))],
+    [sg.Button("0", size=(2,2)), sg.Button(".", size=(2,2)), sg.Button("=", size=(2,2)), sg.Button("%", size=(2,2), button_color=('DarkBlue'))]   
 ]
 
-window = sg.Window("CALCULADORA", layout=layout, element_justification="center")
+window = sg.Window("ANA ELISA ", layout=layout, element_justification="center")
 
 window.read()
