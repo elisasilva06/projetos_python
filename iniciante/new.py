baf3fbf560f5084867d348b20886e93abfecd639
ALEMA <ana.eosilva@aluno.obompastor.com.br>
2025-05-31 15:29:58 -0300

    update

---
+++ iniciante/new.py
@@ -0,0 +1,9 @@
+suits = ["spades","clubs","hearts","diamonds"]
+suit = suits[2]
+suit = "hearts"
+rank = "K"
+value = 10
+print("your card is: " + rank + " of " + suit)
+suits.append("snakes")
+for suit in suits:
+    print(suits)
